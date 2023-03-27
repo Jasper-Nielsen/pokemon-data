6a540b270498cfb801fbc3dc4711934956cf60f7
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-03-27 17:38:58 +0200

    added json and style

--- script.js
+++ script.js
@@ -48,20 +48,28 @@ function displayPokemonInGrid(pokemon) {
   document.querySelector("#pokemon-grid").insertAdjacentHTML(
     "beforeend",
     /*html*/ ` <article class="grid-item">
-                <img src="${pokemon.image}">
+              <div id="grid-image-container">
+                <img id="grid-image" src="${pokemon.image}">
+              </div>
                 <h2>${pokemon.name}</h2>
                 
             </article>   
 `
   );
 
+  // makes item clickable and shows dialogWindow
   document
     .querySelector("#pokemon-grid article:last-child")
     .addEventListener("click", modalWindowClicked);
 
+  // add pokemons on click
   document
     .querySelector("#dialog-window")
     .addEventListener("click", addPokemonToList(pokemon));
+
+  // document
+  //   .querySelector("#pokemon-grid article:last-child")
+  //   .addEventListener("click", applyBackgroundBasedOnType(pokemon));
 }
 
 function modalWindowClicked() {
@@ -99,30 +107,92 @@ function addPokemonToList(pokemon) {
   //     <li> <img id="footprint-image" src=${pokemon.footprint}> </li>
   // </section>
   // </div>
-  //   `;
-  document.querySelector("#pokemon-image").src = pokemon.image;
-  document.querySelector("#name").textContent = pokemon.name;
-  document.querySelector("#description").textContent = pokemon.description;
-  document.querySelector("#ability").textContent = pokemon.ability;
-  document.querySelector("#dexindex").textContent = pokemon.dexindex;
-  document.querySelector("#type").textContent = pokemon.type;
-  document.querySelector("#subtype").textContent = pokemon.subtype;
-  document.querySelector("#weakness").textContent = pokemon.weakness;
-  document.querySelector("#gender").textContent = pokemon.gender;
-  document.querySelector("#weight").textContent = pokemon.weight;
-  document.querySelector("#height").textContent = pokemon.height;
-  document.querySelector("#generation").textContent = pokemon.generation;
-  document.querySelector("#spilversion").textContent = pokemon.name;
-  document.querySelector("#can-evolve").textContent = pokemon.name;
-  document.querySelector("#health-points").textContent = pokemon.name;
-  document.querySelector("#attack").textContent = pokemon.name;
-  document.querySelector("#defense").textContent = pokemon.name;
-  document.querySelector("#special-attack").textContent = pokemon.name;
-  document.querySelector("#special-defense").textContent = pokemon.name;
-  document.querySelector("#speed").textContent = pokemon.name;
+  // //   `;
+  //  document
+  //    .querySelector("#dialog-window")
+  //    .insertAdjacentHTML("beforeend", html);
+
+  document.querySelector("#dialog-image").src = pokemon.image;
+  document.querySelector("#name").textContent = `Name: ${pokemon.name}`;
+  document.querySelector(
+    "#description"
+  ).textContent = `Desription: ${pokemon.description}`;
+  document.querySelector(
+    "#ability"
+  ).textContent = `Abilitíes: ${pokemon.ability}`;
+  document.querySelector(
+    "#dexindex"
+  ).textContent = `Pokedex Number: ${pokemon.dexindex}`;
+  document.querySelector("#type").textContent = `Type: ${pokemon.type}`;
+  document.querySelector(
+    "#subtype"
+  ).textContent = `Subtype: ${pokemon.subtype}`;
+  document.querySelector(
+    "#weakness"
+  ).textContent = `Weakness: ${pokemon.weakness}`;
+
+  document.querySelector(
+    "#gender"
+  ).textContent = `Gender: ${capatilizeFirstLetterGender(pokemon)}`;
+
+  document.querySelector("#weight").textContent = `Weight: ${pokemon.weight}kg`;
+  document.querySelector("#height").textContent = `Height: ${pokemon.height}cm`;
+  document.querySelector(
+    "#generation"
+  ).textContent = `Generation: ${pokemon.generation}`;
+  document.querySelector(
+    "#spilversion"
+  ).textContent = `Gameversion: ${pokemon.spilversion}`;
+
+  document.querySelector("#can-evolve").textContent = `Can evolve: ${canEvolve(
+    pokemon
+  )}`;
+  document.querySelector(
+    "#health-points"
+  ).textContent = `Health points: ${pokemon.statsHP}`;
+  document.querySelector(
+    "#attack"
+  ).textContent = `Attack: ${pokemon.statsAttack}`;
+  document.querySelector(
+    "#defense"
+  ).textContent = `Defence: ${pokemon.statsDefence}`;
+  document.querySelector(
+    "#special-attack"
+  ).textContent = `Special Attack: ${pokemon.statsSpecialAttack}`;
+  document.querySelector(
+    "#special-defense"
+  ).textContent = `Special Defece: ${pokemon.statsSpecialDefence}`;
+  pokemon.statsSpecialDefence;
+  document.querySelector("#speed").textContent = `Speed: ${pokemon.statsSpeed}`;
+  document.querySelector("#footprint-text").textContent=`Footprint: `;
   document.querySelector("#footprint-image").src = pokemon.footprint;
 
-  document
-    .querySelector("#dialog-window")
-    .insertAdjacentHTML("beforeend", html);
+
+
+  applyBackgroundBasedOnType(pokemon);
+}
+
+function capatilizeFirstLetterGender(pokemon) {
+  const firstLetter = pokemon.gender.charAt(0).toUpperCase();
+  const restOfGender = pokemon.gender.substring(1);
+  return `${firstLetter}${restOfGender}`;
+}
+
+function canEvolve(pokemon) {
+  let status;
+  if (pokemon.canEvolve === true) {
+    status = `The pokemon can evolve`;
+  } else status = `The pokemon can't evolve`;
+
+  return status;
+}
+
+function applyBackgroundBasedOnType(pokemon) {
+  if (pokemon.type == `Psychic`) {
+    document.querySelector("#dialog-window").classList.add("psychic-background");
+  } else
+    document.querySelector("#dialog-window").classList.add("blue-background");
+
+  /*
+     Normal, Fire, Water, Grass, Flying, Fighting, Poison, Electric, Ground, Rock, Psychic, Ice, Bug, Ghost, Steel, Dragon, Dark and Fairy */
 }
