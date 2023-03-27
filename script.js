8fcb77628c322110c280804a4796bdc83999d5eb
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-03-27 23:22:16 +0200

    changed json files names

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function initApp() {
 async function getPokemonData(url) {
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+
   return data;
 }
 
